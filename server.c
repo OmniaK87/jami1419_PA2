3ba53537bb082d12f3308a6952a76a937860f650
OmniaK87 <jake.mitchell@colorado.edu>
2017-10-19 21:40:48 -0600

    Config file is in, now on to network portion

--- server.c
+++ server.c
@@ -3,6 +3,9 @@
 * Fall 2017 Networks Programming Assignment 2
 */
 
+
+//Currently have config file in, with values having a leading space.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +19,10 @@ struct keyValue {
     UT_hash_handle hh;
 };
 
-void add_key_value(char* key, char* value);
+
+
+void add_key_value(struct keyValue** , char*, char*);
+struct keyValue *findKey(struct keyValue**, char*);
 
 
 
@@ -26,23 +32,34 @@ int main(int argc, char **argv) {
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
-
+    struct keyValue *hashTable = NULL;
 
     confFile = fopen("ws.conf", "r");
     if (confFile == NULL)
         exit(EXIT_FAILURE);
 
-    struct my_struct *hashTable = NULL;
-    printf("adding first item to hash.\n");
-    add_key_value(*hashTable, "test", "testing");
+
+    /*printf("adding first item to hash.\n");
+    add_key_value(&hashTable, "test", "testing");
     printf("done adding first item to hash.\n");
+    struct keyValue *f = findKey(&hashTable, "test");
+    printf("%s\n", f->key);*/
 
 
     while ((read = getline(&line, &len, confFile)) != -1) {
         if(line[0] != '#'){
-            printf("%s", line);
+            //printf("%s", line);
+            char* second = strchr(line, ' ');
+            size_t lengthOfFirst = second - line;
+            char* first = (char*)malloc((lengthOfFirst + 1)*sizeof(char));
+            strncpy(first, line, lengthOfFirst);
+
+            add_key_value(&hashTable, first, second);
         }
     }
+
+    struct keyValue *f = findKey(&hashTable, ".jpg");
+    printf("%s\n", f->value);
 
     fclose(confFile);
 
@@ -50,12 +67,20 @@ int main(int argc, char **argv) {
 
 void add_key_value(struct keyValue **hash, char* keyIn, char* valueIn) {
     struct keyValue *s;
-    HASH_FIND_INT(hashTable, &keyIn, s);  /* id already in the hash? */
+    HASH_FIND_INT(*hash, &keyIn, s);  //id already in the hash?
     if (s==NULL) {
         s = malloc(sizeof(struct keyValue));
         strcpy(s->key, keyIn);
         strcpy(s->value, valueIn);
-        HASH_ADD_STR(hash, key, s);
+        HASH_ADD_STR(*hash, key, s);
     }
 }
+
+struct keyValue *findKey(struct keyValue **hash, char* keyId) {
+    struct keyValue *s;
+
+    HASH_FIND_STR(*hash, keyId, s);
+
+    return s;
+};
 
